5db0be9aff6ad5ba1c81e7d93ea937f39e6556ab
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-07-13 14:13:27 -0500

    Finished

---
+++ SumOf3/SumOf3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumOf3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first number.");
+            double first = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter your second number.");
+            double second = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter your third number.");
+            double third = Convert.ToDouble(Console.ReadLine());
+
+            double sum = first + second + third;
+
+            Console.WriteLine("The sum is " + sum);
+
+            const double value = 7.777;
+
+            double multiply = value * sum;
+
+            Console.WriteLine($"The sum times 7.777 is {multiply.ToString("N3")}");
+        }
+    }
+}
